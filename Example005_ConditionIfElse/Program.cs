82e3ade835c5832051da42fd5741ac38b6ea93ba
Andrey <avgust1@mail.ru>
2023-10-18 16:53:38 +0500

    Создали папку с IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower переводит введенное слово в нижний регистр
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
