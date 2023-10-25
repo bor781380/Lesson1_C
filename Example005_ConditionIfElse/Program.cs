b4b99184fdd5bf82e52b0a369982a80fe0548ae3
Andrey <avgust1@mail.ru>
2023-10-25 23:15:45 +0500

    Учимся

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
 Console.WriteLine("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша") //ToLower переводит введенное слово в нижний регистр
+if(username.ToLower() == "masha") //ToLower переводит введенное слово в нижний регистр
 {
     Console.WriteLine("Ура, это же Маша!");
 }
